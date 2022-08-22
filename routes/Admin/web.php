c58a269be92f5941896795d889dac99369e34643
Alshrk <email>
2022-08-22 17:51:55 +0200

    first laravel deployment to heroku

--- routes/Admin/web.php
+++ routes/Admin/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::group(['prefix' => 'admin', 'middleware' => ['role:admin|super_admin']], function() {
-    Route::get('/', 'DashboardController@index');
-    Route::get('/manage', ['middleware' => ['permission:manage-admins'], 'uses' => 'AdminController@manageAdmins']);
+Route::middleware('auth:sanctum')->get('/user', function () {
+    return view('welcome');
 });
+
+// Route::group(['prefix' => 'admin', 'middleware' => ['role:admin|super_admin']], function() {
+//     Route::get('/', 'DashboardController@index');
+//     Route::get('/manage', ['middleware' => ['permission:manage-admins'], 'uses' => 'AdminController@manageAdmins']);
+// });
